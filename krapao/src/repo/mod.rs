ecd918b1bad09deb605929baba2fd7822581271a
shigedangao <marc.inthaamnouay@gmail.com>
2022-03-02 18:27:47 +0100

    add dockerfile

--- krapao/src/repo/mod.rs
+++ krapao/src/repo/mod.rs
@@ -12,7 +12,7 @@ pub fn initialize_git(env: &Env) -> Result<GitConfig, Error> {
     // retrieve the environment variable for git credentials
     let credentials = Credentials::new(env);
 
-    let config = GitConfig::new(credentials, &env.repository, &env.target)?;
+    let config = GitConfig::new(credentials, &env.repository, env.target.to_owned())?;
     config.init_repository()?;
 
     Ok(config)
